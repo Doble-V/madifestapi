680ec15ad4d4659c0c20a196f2e7b134bd27b9b6
Doble-V <diegovv26@gmail.com>
2026-03-20 03:28:46 -0500

    Update program with permissions core

--- Program.cs
+++ Program.cs
@@ -3,6 +3,22 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy
+            .WithOrigins(
+                "http://localhost:3000",
+                "https://fest.madistack.com",
+                "https://www.fest.madistack.com",
+                "https://madifest.vercel.app"
+            )
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -13,8 +29,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
+app.UseCors("AllowFrontend");
+
 app.UseSwagger();
 app.UseSwaggerUI();
 
+app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
